f2af3db4f6444fbdc7aa6c534aba11919027548c
Aviral <aviral.jain2020@vitstudent.ac.in>
2022-07-21 23:42:08 +0530

    added features

---
+++ jsonGenerator.py
@@ -0,0 +1,32 @@
+import csv
+import json
+
+hostel=0
+mess=0
+type_dict={1:"MenuType.BREAKFAST",2:"MenuType.LUNCH",3:"MenuType.SNACK",4:"MenuType.DINNER"}
+hostel_dict={1:"MH", 2:"LH"}
+mess_dict={1:"Special", 2:"Veg", 3:"NonVeg"}
+
+hostel=int(input("Select the hostel type:\n1. MH\n2. LH\n"))
+mess=int(input("Select the mess type:\n1. Special\n2. Veg\n3. Non-veg\n"))
+menu=input("Enter the menu file path:\n")
+
+if (hostel>2 or mess>3 or hostel<1 or mess<1):
+    print("Invalid input")
+    exit()
+
+json_menu={"hostel":hostel,"mess":mess,"menu":[]}
+
+with open(menu) as file_obj:
+    reader_obj=csv.reader(file_obj)
+    for row in reader_obj:
+        menu_json={}
+        menu_json["date"]=row[0]
+        menu_json["menu"]=[]
+        for i in range(1,len(row)):
+            menu_json["menu"].append({"type":type_dict[i],"menu":row[i].replace('\n','')})
+        json_menu["menu"].append(menu_json)
+
+# save as json file
+with open('menu-'+hostel_dict[hostel]+'-'+mess_dict[mess]+'.json','w') as file_obj:
+    json.dump(json_menu,file_obj)
